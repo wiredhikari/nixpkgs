9aa6dad24b68b178c2e16e4af7b48bc78e5fb389
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-02 22:42:13 +0100

    python3Packages.pycognito: 2022.01.0 -> 2022.02.1

--- pkgs/development/python-modules/pycognito/default.nix
+++ pkgs/development/python-modules/pycognito/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "pycognito";
-  version = "2022.01.0";
+  version = "2022.02.1";
   disabled = isPy27;
 
   src = fetchFromGitHub {
     owner = "pvizeli";
     repo = pname;
     rev = version;
-    sha256 = "sha256-mmlw3irMC0SFjfEinXHyoPNfTvCcO02zGyqQLj9STSY=";
+    sha256 = "sha256-0PqeZ8yy2MzvIi1xQNosR7V2Ma3tMT0Q/v4OIv7f1Kg=";
   };
 
   propagatedBuildInputs = [
