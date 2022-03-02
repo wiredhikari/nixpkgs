4f6b08ac2e731a6b409a329a0d87c8e0a90d6803
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-02 22:42:15 +0100

    python3Packages.pymongo: 3.12.2 -> 4.0.1

--- pkgs/development/python-modules/pymongo/default.nix
+++ pkgs/development/python-modules/pymongo/default.nix
@@ -6,12 +6,12 @@
 
 buildPythonPackage rec {
   pname = "pymongo";
-  version = "3.12.2";
+  version = "4.0.1";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "64ea5e97fca1a37f83df9f3460bf63640bc0d725e12f3471e6acbf3a6040dd37";
+    sha256 = "sha256-E9BiTBOpHacfoNlgIF2Ts9mDREgb6GXufMI4yXLUHXM=";
   };
 
   # Tests call a running mongodb instance
